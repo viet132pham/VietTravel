f57627562c757d57a7bfd76d84b2c4c116ceb7dc
viet132pham <viet132pham@gmail.com>
2023-08-01 16:43:03 +0700

    fix contact page - about us

---
+++ frontend/doan/src/components/Pages/components/AboutUs/index.js
@@ -0,0 +1,285 @@
+import React from "react";
+import "./index.scss";
+import HeaderNav from "../../../commons/HeaderNav/HeaderNav";
+
+function AboutUs(props) {
+  return (
+    <div className="about-us-wrapper">
+      <HeaderNav />
+      <hr />
+      <div className="about-us-content-wrapper">
+        <div className="row-1 col-12 d-flex">
+          <div className="col-6">
+            <div className="col-10 mb-3">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci
+              animi et eum ipsum iste obcaecati quaerat voluptate.
+            </div>
+            <div className="col-12">
+              Architecto asperiores assumenda, dignissimos dolor.
+            </div>
+          </div>
+          <div className="col-6 d-flex">
+            <div className="col-6">
+              <div className="icon mb-2">
+                <i class="fa-solid fa-earth-asia"></i>
+              </div>
+              <div
+                className="mb-2"
+                style={{ fontSize: "20px", fontWeight: "700px" }}
+              >
+                Understand the World
+              </div>
+              <div> Architecto asperiores assumenda, dignissimos dolor.</div>
+            </div>
+            <div className="col-6" style={{ marginLeft: "24px" }}>
+              <div className="icon mb-2">
+                <i class="fa-solid fa-image"></i>
+              </div>
+              <div
+                className="mb-2"
+                style={{ fontSize: "20px", fontWeight: "700px" }}
+              >
+                Understand the World
+              </div>
+              <div> Architecto asperiores assumenda, dignissimos dolor.</div>
+            </div>
+          </div>
+        </div>
+        <hr />
+        <div className="row-1 col-12 d-flex" style={{ marginTop: "30px" }}>
+          <div className="col-6">
+            <div className="col-10 mb-3">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci
+              animi et eum ipsum iste obcaecati quaerat voluptate.
+            </div>
+            <div className="col-12">
+              Architecto asperiores assumenda, dignissimos dolor.
+            </div>
+          </div>
+          <div className="col-6 d-flex">
+            <div className="col-4">
+              <div
+                className="icon mb-2"
+                style={{ width: "200px", height: "135px" }}
+              >
+                <img
+                  width="100%"
+                  src="https://mobirise.com/extensions/postm5/artworks-exhibitions-news/assets/images/image13.jpg"
+                  alt="anh-1"
+                />
+              </div>
+              <div className="icon" style={{ width: "200px", height: "135px" }}>
+                <img
+                  width="100%"
+                  src="https://mobirise.com/extensions/postm5/artworks-exhibitions-news/assets/images/image13.jpg"
+                  alt="anh-1"
+                />
+              </div>
+            </div>
+            <div className="col-8" style={{ marginLeft: "24px" }}>
+              <img
+                width="100%"
+                src="https://mobirise.com/extensions/postm5/artworks-exhibitions-news/assets/images/image10.jpg"
+                alt="anh-3"
+              />
+            </div>
+          </div>
+        </div>
+        <hr />
+        <div className="row-1 col-12" style={{ marginTop: "30px" }}>
+          <div
+            className="col-12 d-flex justify-content-center"
+            style={{
+              fontSize: "26px",
+              fontWeight: "700",
+              marginTop: "30px",
+              marginBottom: "30px",
+            }}
+          >
+            Review
+          </div>
+          <div className="col-12 d-flex">
+            <div className="col-4">
+              <img
+                width="100%"
+                src="https://mobirise.com/extensions/postm5/artworks-exhibitions-news/assets/images/image10.jpg"
+                alt="anh-3"
+              />
+              <div className="mt-4">
+                Adipisci aperiam at autem commodi cumque eligendi eos.
+              </div>
+            </div>
+            <div className="col-4" style={{ margin: "0 16px" }}>
+              <img
+                width="100%"
+                src="https://mobirise.com/extensions/postm5/artworks-exhibitions-news/assets/images/image15.jpg"
+                alt="anh-3"
+              />
+              <div className="mt-4">
+                Adipisci aperiam at autem commodi cumque eligendi eos.
+              </div>
+            </div>
+            <div className="col-4">
+              <img
+                width="100%"
+                src="https://mobirise.com/extensions/postm5/artworks-exhibitions-news/assets/images/image16.jpg"
+                alt="anh-3"
+              />
+              <div className="mt-4">
+                Adipisci aperiam at autem commodi cumque eligendi eos.
+              </div>
+            </div>
+          </div>
+        </div>
+        <hr />
+        <div className="col-12">
+          <div
+            className="col-12 d-flex justify-content-center"
+            style={{
+              fontSize: "26px",
+              fontWeight: "700",
+              marginTop: "30px",
+              marginBottom: "30px",
+              color: "#6faa41",
+            }}
+          >
+            About Us
+          </div>
+          <div className="col-12 d-flex justify-content-between">
+            <div className="col-5">
+              <img
+                width="100%"
+                src="https://png.pngitem.com/pimgs/s/571-5717205_conversation-hd-png-download.png"
+              />
+            </div>
+            <div className="col-5">
+              <div
+                className="mb-4"
+                style={{
+                  fontSize: "20px",
+                  fontWeight: "700",
+                  fontStyle: "italic",
+                  color: "#6faa41",
+                }}
+              >
+                Chúng tôi giúp bạn
+              </div>
+              <div className="item mt-2 mb-1">
+                ► bal bạ sjkha skuhf ,àksa aduka âk fkag
+              </div>
+              <div className="item mt-2 mb-1">
+                ► bal bạ sjkha skuhf ,àksa aduka âk fkag
+              </div>
+              <div className="item mt-2 mb-1">
+                ► bal bạ sjkha skuhf ,àksa aduka âk fkag
+              </div>
+              <div className="item mt-2 mb-1">
+                ► bal bạ sjkha skuhf ,àksa aduka âk fkag
+              </div>
+            </div>
+          </div>
+        </div>
+        <hr />
+        <div className="col-12" style={{ marginBottom: "100px" }}>
+          <div
+            className="col-12 d-flex justify-content-center"
+            style={{
+              fontSize: "26px",
+              fontWeight: "700",
+              marginTop: "30px",
+              marginBottom: "30px",
+              fontStyle: "italic",
+              color: "#fb853e",
+            }}
+          >
+            Contact Us
+          </div>
+          <div className="col-12 d-flex justify-content-between">
+            <div className="col-5" style={{ paddingLeft: "150px" }}>
+              <div className="col-10">
+                <div
+                  style={{
+                    margin: "15px 0",
+                    color: "#1e889e",
+                    fontWeight: "600",
+                  }}
+                >
+                  <span style={{ marginRight: "8px" }}>
+                    <i class="fa-solid fa-location-dot"></i>
+                  </span>
+                  <span> Địa chỉ</span>
+                </div>
+                <div style={{ textIndent: "15px", fontWeight: "400" }}>
+                  Số 1 Đại học bách khoa
+                </div>
+              </div>
+              <div className="col-10">
+                <div
+                  style={{
+                    margin: "15px 0",
+                    color: "#1e889e",
+                    fontWeight: "600",
+                  }}
+                >
+                  <span style={{ marginRight: "8px" }}>
+                    <i class="fa-solid fa-envelope"></i>
+                  </span>
+                  <span>Email</span>
+                </div>
+                <div style={{ textIndent: "15px", fontWeight: "400" }}>
+                  backkhoa@gmail.com
+                </div>
+              </div>
+              <div className="col-10">
+                <div
+                  style={{
+                    margin: "15px 0",
+                    color: "#1e889e",
+                    fontWeight: "600",
+                  }}
+                >
+                  <span style={{ marginRight: "8px" }}>
+                    <i class="fa-solid fa-phone"></i>
+                  </span>
+                  <span>Liên hệ</span>
+                </div>
+                <div style={{ textIndent: "15px", fontWeight: "400" }}>
+                  0152478965
+                </div>
+              </div>
+            </div>
+            <div className="col-5">
+              <div className="col-12">
+                <div
+                  style={{
+                    fontSize: "18px",
+                    fontStyle: "italic",
+                    color: "#1e889e",
+                    fontWeight: "600",
+                  }}
+                >
+                  Get in touch
+                </div>
+                <div className="col-12 d-flex" style={{marginTop: '24px'}}>
+                  <div className="col-6">
+                    <input placeholder="Full Name" />
+                  </div>
+                  <div className="col-6">
+                    <input placeholder="Email" />
+                  </div>
+                </div>
+                <div className="col-12" style={{marginTop: '24px'}}>
+                  <textarea placeholder="Message" />
+                </div>
+                <div className="col-12" style={{marginTop: '24px'}}>
+                  <button>Gửi</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default AboutUs;
