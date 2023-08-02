e0c6143534aa2c3bf80a8c9bee1dcf9f2cb20fe1
viet132pham <viet132pham@gmail.com>
2023-08-02 11:41:58 +0700

    about page

--- frontend/doan/src/components/Pages/components/AboutUs/index.js
+++ frontend/doan/src/components/Pages/components/AboutUs/index.js
@@ -11,37 +11,36 @@ function AboutUs(props) {
         <div className="row-1 col-12 d-flex">
           <div className="col-6">
             <div className="col-10 mb-3">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci
-              animi et eum ipsum iste obcaecati quaerat voluptate.
+              VietTravel là một trang web đặt tour du lịch hàng đầu với mục tiêu mang đến những trải nghiệm tuyệt vời cho du khách khám phá thế giới.
             </div>
             <div className="col-12">
-              Architecto asperiores assumenda, dignissimos dolor.
+              Với hơn 10 năm kinh nghiệm, chúng tôi tự hào là đối tác đáng tin cậy của hàng triệu du khách trên khắp thế giới.
             </div>
           </div>
           <div className="col-6 d-flex">
             <div className="col-6">
-              <div className="icon mb-2">
+              <div className="icon mb-2" style={{ color: "blue" }}>
                 <i class="fa-solid fa-earth-asia"></i>
               </div>
               <div
                 className="mb-2"
                 style={{ fontSize: "20px", fontWeight: "700px" }}
               >
-                Understand the World
+                Hiểu biết thế giới
               </div>
-              <div> Architecto asperiores assumenda, dignissimos dolor.</div>
+              <div>Chúng tôi có đội ngũ chuyên gia du lịch tận tâm và nhiệt huyết, luôn sẵn sàng chia sẻ kiến thức về các điểm đến trên toàn thế giới.</div>
             </div>
             <div className="col-6" style={{ marginLeft: "24px" }}>
-              <div className="icon mb-2">
+              <div className="icon mb-2" style={{ color: "palevioletred" }}>
                 <i class="fa-solid fa-image"></i>
               </div>
               <div
                 className="mb-2"
                 style={{ fontSize: "20px", fontWeight: "700px" }}
               >
-                Understand the World
+                Trải nghiệm đáng nhớ
               </div>
-              <div> Architecto asperiores assumenda, dignissimos dolor.</div>
+              <div>Chúng tôi cam kết mang đến những trải nghiệm du lịch đáng nhớ và khó quên cho mỗi khách hàng.</div>
             </div>
           </div>
         </div>
@@ -49,29 +48,28 @@ function AboutUs(props) {
         <div className="row-1 col-12 d-flex" style={{ marginTop: "30px" }}>
           <div className="col-6">
             <div className="col-10 mb-3">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci
-              animi et eum ipsum iste obcaecati quaerat voluptate.
+              Chúng tôi luôn lắng nghe ý kiến của khách hàng và không ngừng cải tiến để mang đến dịch vụ tốt nhất.
             </div>
             <div className="col-12">
-              Architecto asperiores assumenda, dignissimos dolor.
+              Đến với VietTravel, bạn sẽ cảm nhận được sự chuyên nghiệp, uy tín và đáng tin cậy từ đội ngũ nhân viên của chúng tôi.
             </div>
           </div>
           <div className="col-6 d-flex">
             <div className="col-4">
               <div
-                className="icon mb-2"
-                style={{ width: "200px", height: "135px" }}
+                className="icon"
+                style={{ width: "200px", height: "126px" }}
               >
                 <img
                   width="100%"
-                  src="https://mobirise.com/extensions/postm5/artworks-exhibitions-news/assets/images/image13.jpg"
+                  src="https://agent.rever.vn/hubfs/Cham-soc-khach-hang-01.jpg"
                   alt="anh-1"
                 />
               </div>
               <div className="icon" style={{ width: "200px", height: "135px" }}>
                 <img
                   width="100%"
-                  src="https://mobirise.com/extensions/postm5/artworks-exhibitions-news/assets/images/image13.jpg"
+                  src="https://doanhnhanplus.vn/wp-content/uploads/2019/12/dnp-nghe-thuat-lang-nghe-khach-hang-1.jpg"
                   alt="anh-1"
                 />
               </div>
@@ -79,7 +77,7 @@ function AboutUs(props) {
             <div className="col-8" style={{ marginLeft: "24px" }}>
               <img
                 width="100%"
-                src="https://mobirise.com/extensions/postm5/artworks-exhibitions-news/assets/images/image10.jpg"
+                src="https://media.vietravel.com/images/news/vietravel-co-uy-tin-khong223.jpg"
                 alt="anh-3"
               />
             </div>
@@ -96,37 +94,37 @@ function AboutUs(props) {
               marginBottom: "30px",
             }}
           >
-            Review
+            Nhận xét từ khách hàng
           </div>
           <div className="col-12 d-flex">
             <div className="col-4">
-              <img
+              <img style={{borderRadius: "50%" }}
                 width="100%"
-                src="https://mobirise.com/extensions/postm5/artworks-exhibitions-news/assets/images/image10.jpg"
+                src="https://img5.thuthuatphanmem.vn/uploads/2021/08/25/anh-dai-dien-an-danh_085759839.jpg"
                 alt="anh-3"
               />
               <div className="mt-4">
-                Adipisci aperiam at autem commodi cumque eligendi eos.
+                "Tôi đã có một chuyến du lịch tuyệt vời cùng với VietTravel. Dịch vụ tuyệt vời và nhân viên nhiệt tình."
               </div>
             </div>
             <div className="col-4" style={{ margin: "0 16px" }}>
-              <img
+              <img style={{borderRadius: "50%" }}
                 width="100%"
-                src="https://mobirise.com/extensions/postm5/artworks-exhibitions-news/assets/images/image15.jpg"
+                src="https://taytou.com/wp-content/uploads/2022/08/Anh-Avatar-dai-dien-mac-dinh-nu-nen-hong.jpg"
                 alt="anh-3"
               />
               <div className="mt-4">
-                Adipisci aperiam at autem commodi cumque eligendi eos.
+                "Chúng tôi đã có một trải nghiệm du lịch tuyệt vời cùng với VietTravel. Sẽ quay lại trong tương lai."
               </div>
             </div>
             <div className="col-4">
-              <img
+              <img style={{borderRadius: "50%" }}
                 width="100%"
-                src="https://mobirise.com/extensions/postm5/artworks-exhibitions-news/assets/images/image16.jpg"
+                src="https://scr.vn/wp-content/uploads/2020/07/Avatar-m%E1%BA%B7c-%C4%91%E1%BB%8Bnh-m%C3%A0u-s%E1%BA%AFc.jpg"
                 alt="anh-3"
               />
               <div className="mt-4">
-                Adipisci aperiam at autem commodi cumque eligendi eos.
+                "VietTravel là lựa chọn số một cho những chuyến du lịch của chúng tôi. Chúng tôi rất hài lòng với dịch vụ của họ."
               </div>
             </div>
           </div>
@@ -143,7 +141,7 @@ function AboutUs(props) {
               color: "#6faa41",
             }}
           >
-            About Us
+            Về chúng tôi
           </div>
           <div className="col-12 d-flex justify-content-between">
             <div className="col-5">
@@ -165,16 +163,16 @@ function AboutUs(props) {
                 Chúng tôi giúp bạn
               </div>
               <div className="item mt-2 mb-1">
-                ► bal bạ sjkha skuhf ,àksa aduka âk fkag
+                ► Tận hưởng những chuyến du lịch tuyệt vời cùng với VietTravel.
               </div>
               <div className="item mt-2 mb-1">
-                ► bal bạ sjkha skuhf ,àksa aduka âk fkag
+                ► Khám phá thế giới và nhận được những trải nghiệm đáng nhớ.
               </div>
               <div className="item mt-2 mb-1">
-                ► bal bạ sjkha skuhf ,àksa aduka âk fkag
+                ► Tự tin lựa chọn VietTravel là đối tác du lịch tin cậy.
               </div>
               <div className="item mt-2 mb-1">
-                ► bal bạ sjkha skuhf ,àksa aduka âk fkag
+                ► Nhận được dịch vụ chuyên nghiệp và tận tâm từ đội ngũ nhân viên của chúng tôi.
               </div>
             </div>
           </div>
@@ -192,7 +190,7 @@ function AboutUs(props) {
               color: "#fb853e",
             }}
           >
-            Contact Us
+            Liên hệ chúng tôi
           </div>
           <div className="col-12 d-flex justify-content-between">
             <div className="col-5" style={{ paddingLeft: "150px" }}>
@@ -227,7 +225,7 @@ function AboutUs(props) {
                   <span>Email</span>
                 </div>
                 <div style={{ textIndent: "15px", fontWeight: "400" }}>
-                  backkhoa@gmail.com
+                  contact@viettravel.com
                 </div>
               </div>
               <div className="col-10">
@@ -244,7 +242,7 @@ function AboutUs(props) {
                   <span>Liên hệ</span>
                 </div>
                 <div style={{ textIndent: "15px", fontWeight: "400" }}>
-                  0152478965
+                  0123 456 789
                 </div>
               </div>
             </div>
@@ -258,18 +256,18 @@ function AboutUs(props) {
                     fontWeight: "600",
                   }}
                 >
-                  Get in touch
+                  Liên hệ với chúng tôi
                 </div>
                 <div className="col-12 d-flex" style={{marginTop: '24px'}}>
                   <div className="col-6">
-                    <input placeholder="Full Name" />
+                    <input placeholder="Họ và tên" />
                   </div>
                   <div className="col-6">
                     <input placeholder="Email" />
                   </div>
                 </div>
                 <div className="col-12" style={{marginTop: '24px'}}>
-                  <textarea placeholder="Message" />
+                  <textarea placeholder="Nội dung" />
                 </div>
                 <div className="col-12" style={{marginTop: '24px'}}>
                   <button>Gửi</button>
